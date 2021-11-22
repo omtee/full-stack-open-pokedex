6c2b57d9fa32d3c576d3c6a8139fd2bdeee29eaf
Olli Tikka <omtikka87@gmail.com>
2021-11-22 14:22:25 +0200

    11.9 second e2e test

--- cypress/integration/pokedex.cypress.js
+++ cypress/integration/pokedex.cypress.js
@@ -3,5 +3,11 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href*="/pokemon/ivysaur"]').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
   })
 })
