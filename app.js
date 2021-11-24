64a042cf94a91cdb8016ef0403aa615e0c8c7ba8
Olli Tikka <omtikka87@gmail.com>
2021-11-24 14:41:23 +0200

    11.15 deploy only for master

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v8') // change this string to ensure a new version deployed
+  res.send('v10') // change this string to ensure a new version deployed
 })
