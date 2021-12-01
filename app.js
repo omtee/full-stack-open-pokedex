c24ac096e04d2e731a9541c7fde4fdf5ce9cafda
Olli Tikka <omtikka87@gmail.com>
2021-12-01 11:36:07 +0200

    update version

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v11') // change this string to ensure a new version deployed
+  res.send('v12') // change this string to ensure a new version deployed
 })
